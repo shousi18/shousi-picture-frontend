e1e6ba173181ce1bdf2d7862c3adf21222229959
贺羿鼎 <13204855+shou_si@user.noreply.gitee.com>
2025-03-22 20:03:13 +0800

    初始化项目，使用ant-design-vue、vite+vue3

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="basicLayout">
+    <a-layout>
+      <a-layout-header>Header</a-layout-header>
+      <a-layout-content>Content</a-layout-content>
+      <a-layout-footer>Footer</a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped></style>
