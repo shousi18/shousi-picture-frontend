827bf6af542c358cf9adbdc808cf982968897281
shousi18 <1416239139@qq.com>
2025-05-15 23:48:21 +0800

    Revert "fix:修改一些bug；"
    This reverts commit ef8361a34e74a5108a542ead0731cb4332e67a1b.

--- src/api/pictureController.ts
+++ src/api/pictureController.ts
@@ -244,37 +244,19 @@ export async function uploadPicture(
   // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
   params: API.uploadPictureParams,
   body: {},
-  file: File,
   options?: { [key: string]: any }
 ) {
-  const formData = new FormData()
-
-  if (file) {
-    formData.append('file', file)
-  }
-
-  Object.keys(body).forEach((ele) => {
-    const item = (body as any)[ele]
-
-    if (item !== undefined && item !== null) {
-      if (typeof item === 'object' && !(item instanceof File)) {
-        if (item instanceof Array) {
-          item.forEach((f) => formData.append(ele, f || ''))
-        } else {
-          formData.append(ele, JSON.stringify(item))
-        }
-      } else {
-        formData.append(ele, item)
-      }
-    }
-  })
-
   return request<API.BaseResponsePictureVO>('/picture/upload', {
     method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
     params: {
       ...params,
+      pictureUploadRequest: undefined,
+      ...params['pictureUploadRequest'],
     },
-    data: formData,
+    data: body,
     ...(options || {}),
   })
 }
